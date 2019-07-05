8d13061facd01069e112fd55bc2ecec1465232ef
Zhyblx <zhangyibinlx@outlook.com>
2019-07-05 14:30:16 +0800

    更新内容如下：1.为ButtonDemo增加了代码说明和注释。2.修改md文件优化了项目说明

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # lejosdemo
 
 
- 最近，在通过lejOS进行学习嵌入式开发。
+通过lejOS进行学习嵌入式开发。
 
 ----
 <b>硬件准备：</b><br>
 <br>
 1.EV3主机<br>
-2.16GB Micro SD卡(完成lejos的安装)<br>
-3.电脑(我选择安装ubuntu系统）<br>
+2.16GB Micro SD卡(需完成烧录lejos)<br>
+3.电脑<br>
 4.USB及RJ12线缆<br>
 5.各类传感器(如：红外传感器、摄像头、电机等)<br>
+<br>
+<br>
 
 <b>软件准备：</b><br>
 <br>
@@ -20,5 +22,10 @@
 4.Java嵌入式运行环境(JRE)：Oracle Java SE Embedded version 7 Update 75<br>
 5.LeJOS<br>
 
-###### 备注：必须使用JDK7，原因Java嵌入式运行环境Oracle只支持到JDK7
+###### 备注：<br> 1.必须使用JDK7，原因Java嵌入式运行环境Oracle只支持到JDK7。<br> 2.电脑系统我选择的是ubuntu18.04(主要因为个人喜欢liunx,【lejos官方没有给出ubuntu系统的配置教程；所以，不想折腾的同学请绕行自Windows或macOS】)。
 
+----
+<b>--学习日志--</b> <br>
+1.LCD(2019年7月3日) <br>
+2.Motor(2019年7月3日) <br>
+3.Button((2019年7月4日)) <br>
--- src/legoev3/demo/button/ButtonDemo.java
+++ src/legoev3/demo/button/ButtonDemo.java
@@ -7,7 +7,11 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * 类：ButtonDemo 作用：侦听某个按钮的按下事件 说明：验证完成；7月4日完成文档注释
+ * 类：ButtonDemo 作用：侦听某个按钮的按下事件(即，click事件) 
+ * 
+ * API地址：
+ * 	Button(类)：http://www.lejos.org/ev3/docs/lejos/hardware/Button.html
+ * 	Keys(接口)：http://www.lejos.org/ev3/docs/lejos/hardware/Keys.html
  * 
  * @author zhangyibin
  *
@@ -19,20 +23,22 @@ public class ButtonDemo {
 	private static TimerTask timerTask = new TimerTask() {
 		@Override
 		public void run() {
-//			System.out.println(new Thread().getName());
-			int i = 500;
+//			System.out.println(new Thread().getName()); // 打印当前run()方法所执行的线程名称
+			int i = 500; // 设置单位时间(秒)
 			while (i >= 0) {
 				try {
-					Thread.sleep(100);
+					Thread.sleep(1000); // 延迟1秒
 
 				} catch (Exception e) {
 					e.printStackTrace();
 
 				}
-//				System.out.println(i);
+				/*
+				 * 当倒计时等于1时，计时结束，同时暴力退出程序
+				 */
 				if (i == 1) {
 					timer.cancel();
-					System.exit(0);
+					System.exit(0); // 暴力退出程序
 
 				}
 				i--;
@@ -41,24 +47,32 @@ public class ButtonDemo {
 	};
 
 	public static void main(String[] args) {
-//		System.out.println(new Thread().getName());
-		timer.schedule(timerTask, 0);
-
+//		System.out.println(new Thread().getName()); // 打印当前main()方法所执行的线程名称
 		/*
-		 *  按钮的Key编号:
+		 * ------------------------------ 
+		 *  按钮KeyID(即,键位ID)列表:
 		 *  ENTER:2 
 		 *  ESCAPE:32
 		 *  LEFT:16 
 		 *  RIGHT:8 
 		 *  UP:1 
 		 *  DOWN:4
+		 * ------------------------------ 
+		 * 1.Button(类)是一个私有化构造类；waitForAnyPress()是Button(类)中定义的一个全局int类型方法，其作用于“返回”你当前所按下“按钮”的keyID(即,键位ID)
+		 * 2.通过waitForAnyPress()方法返回的键位ID来进行判断当前按下的是哪一个按键
+		 * 3.因为Java代码的执行过程是自上而下的，并且执行一次便结束了。所以，为了实现不断的侦听按钮按下事件；程序中通过while循环进行不断的循环侦听。
+		 * 4.Demo中为了保障程序的“安全性”；防止因使用while死循环一直对按钮事件无休止的侦听下去；代码中通过TimerTask开启一条(单位时间)的线程来保障程序可以正常的结束。
 		 * 
 		 */
+		timer.schedule(timerTask, 0); // 使用对象timer中的schedule()方法开启线程并执行run方法中的内容
 		while (true) {
-			int buttonKey = Button.waitForAnyPress();
+			int buttonKey = Button.waitForAnyPress(); //返回当前所按下“按钮”的keyID
+			/*
+			 * 如果侦听到当前按下的是ENTER按钮，那么屏幕中打印ENTER
+			 */
 			if (buttonKey == Keys.ID_ENTER) {
-				LCD.clear(2);
-				LCD.drawString("ENTER", 0, 2);
+				LCD.clear(2); // 清空EV3的屏幕显示
+				LCD.drawString("ENTER", 0, 2);  // EV3屏幕显示内容
 
 			} else if (buttonKey == Keys.ID_ESCAPE) {
 				LCD.clear(2);
