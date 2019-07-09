c9ecc5136f9de143a52067aa8911fb63fd0d4037
Zhyblx <zhangyibinlx@outlook.com>
2019-07-09 19:50:51 +0800

    1.正式完成对红外传感器的研究。2.同时对红外传感器的距离探测实现代码进行了注释

--- src/legoev3/demo/sensor/IRSensorDemo.java
+++ src/legoev3/demo/sensor/IRSensorDemo.java
@@ -1,26 +1,12 @@
 package legoev3.demo.sensor;
 
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorMode;
-import lejos.hardware.sensor.SensorModes;
-import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 /**
- * 背景说明： 1.红外传感器(InfraRedSensor) 2.红外传感器的作用测距、定位。
- * 
- * API地址：
- * EV3IRSensor(类):http://www.lejos.org/ev3/docs/lejos/hardware/sensor/EV3IRSensor.html
- * SensorMode(接口):http://www.lejos.org/ev3/docs/lejos/hardware/sensor/SensorMode.html
- * Port(接口):http://www.lejos.org/ev3/docs/lejos/hardware/port/Port.html
- * SensorPort(接口):http://www.lejos.org/ev3/docs/lejos/hardware/port/SensorPort.html
- * 
- * 
+ * 类：IRSensorDemo  作用：通过EV3提供的红外传感器，完成测量与前方物体的距离
  * 
  * @author zhangyibin
  *
@@ -29,57 +15,36 @@ import lejos.utility.Delay;
 public class IRSensorDemo {
 
 	public static void main(String[] args) {
+		
 		/*
-		 * 获取端口实例:getPort("S4")返回请求端口名称的端口对象
-		 * EV3传感器端口的基本接口:S1(EV3端口1)\S2(EV3端口2)\S3(EV3端口3)\S4(EV3端口4)
-		 * 
+		 * EV3IRSensor(类):EV3红外线传感器实现类；构造方法中的参数是代表“端口号” 
+		 * SensorPort(接口):EV3传感器端口的基本接口；示例：S1(EV3端口1)\S2(EV3端口2)\S3(EV3端口3)\S4(EV3端口4)
+		 * SensorMode(接口):该接口抽象了“传感器”模式的实现；
 		 * 
 		 */
-//		Port port = LocalEV3.get().getPort(String.valueOf(SensorPort.S4));
+		
+		@SuppressWarnings("resource")
+		EV3IRSensor iRSensor = new EV3IRSensor(SensorPort.S1);//EV3红外线传感器实现类；构造方法中的参数是代表“端口号” 
+		SensorMode sensorMode = iRSensor.getDistanceMode();// 调用getDistanceMode()方法表示开启“外线传感器距离模式”；并且测量距传感器与前方物体的距离；该方法返回的数据类型为SensorMode
+		float[] floatSensorMode = new float[sensorMode.sampleSize()]; // 通过sampleSize()方法，返回传感器采集到的“测量距离”个数；每个采集到的“距离”通过数组的方式进行存储
+		System.out.println(floatSensorMode.length);
 
+		sensorMode.fetchSample(floatSensorMode, 0);// 从数组中取出采集的"距离"数
 		/*
-		 * EV3IRSensor(类): EV3红外线传感器实现类；构造方法中的参数是代表 端口号 SensorMode(接口)
-		 * 传感器模式接口；作用于存储getDistanceMode()方法返回的测量距离(测量距传感器前方物体的距离)
-		 * getDistanceMode()方法的返回值是SensorMode类型。
-		 * 
+		 * 每一个采集的“距离”数都通过数组元素输出的方式呈现
 		 */
-		EV3IRSensor iRSensor = new EV3IRSensor(SensorPort.S1);
-		SensorMode sensorMode = iRSensor.getDistanceMode();// EV3红外线传感器探测距离模式，测量距传感器前方物体的距离。
+		for (int i = 0; i < floatSensorMode.length; i++) {
+			System.out.println("探测距离是：" + floatSensorMode[i]);
 
-		float[] floatSensorMode = new float[sensorMode.sampleSize()];
-
-		int i = 5;
-		while (i > 0) {
-			try {
-				Thread.sleep(1000);
-				sensorMode.fetchSample(floatSensorMode, 0);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-
-			}
-
-			for (float num : floatSensorMode) {
-				System.out.println("探测距离是：" + num);
-
-			}
-
-			if (i == 1) {
-				iRSensor.close();
-
-			}
-			i--;
 		}
 
-//			LCD.drawString(String.valueOf((sensorMode.sampleSize())), 0, 5);
-
-//		sensorMode.sampleSize();// 返回测量的距离数值
-
-//		LCD.drawString(String.valueOf(iRSensor.IR_CHANNELS), 0, 4);// 返回具体的端口
-//		iRSensor.getDistanceMode();
-//		LCD.drawString(String.valueOf(iRSensor.getDistanceMode()), 0, 0);
-//		iRSensor.getAvailableModes();
-		Delay.msDelay(1000);
+//		System.out.println(floatSensorMode[0]);
+//
+//		for (float floatNum : floatSensorMode) {
+//			System.out.println(floatNum);
+//
+//		}
+		Delay.msDelay(10000);
 
 	}
 }
