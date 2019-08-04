7dbc0ce733b3bbd4e9ddee391b174461b15d183b
Zhyblx <zhangyibinlx@outlook.com>
2019-08-04 22:39:19 +0800

    1.mark3加入倒计时功能，作用于明确可遥控的时间点。2.优化转向遥控的代码。

--- src/legoev3/Example/Robot/MARK_3/Telecar.java
+++ src/legoev3/Example/Robot/MARK_3/Telecar.java
@@ -23,82 +23,106 @@ import lejos.utility.Delay;
 
 public class Telecar implements Runnable {
 
-	private EV3IRSensor iRSensor = new EV3IRSensor(SensorPort.S1);//
-
+	private EV3IRSensor iRSensor = new EV3IRSensor(SensorPort.S1);
 	private RegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
 	private RegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
 
 	@Override
 	public void run() {
-		motorC.setSpeed(300);// 设置电机C的转速
-		motorB.setSpeed(300);// 设置电机B的转速
+		int i = 5;
+		while (true) {
+			try {
+				Thread.sleep(1000);
+				LCD.drawString("sleep:" + String.valueOf(i), 0, 0);
 
-		while (Button.DOWN.isDown() != true) {
-			byte[] commandArrays = new byte[4];
-			iRSensor.getRemoteCommands(commandArrays, 0, 4);
-			// int i = 0; // 变量i 用于标记按下的次数
-			for (float commandNum : commandArrays) {
-				if (commandNum != 0) {
-					if (commandNum == 1) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorC.forward();// 前进
-						motorB.forward();
+			} catch (Exception e) {
+				e.printStackTrace();
 
-					} else if (commandNum == 3) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorC.backward();// 倒退
-						motorB.backward();
-
-					} else if (commandNum == 9) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorC.stop();// 停止
-						motorB.stop();
-
-					} else if (commandNum == 10) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorC.rotate(200);// 向前 向左转
-
-					} else if (commandNum == 6) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorB.rotate(200);// 向前 向右转
-
-					} else if (commandNum == 7) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorC.rotate(200);// 向后 向左转
-
-					} else if (commandNum == 11) {
-						LCD.clear(2);
-						LCD.drawString(String.valueOf(commandNum), 10, 2);
-						motorB.rotate(200);// 向后 向右转
-
-					} else if (commandNum == 5) {
-						motorC.setSpeed(500);// 设置电机C的转速
-						motorB.setSpeed(500);// 设置电机B的转速
-						
-
-					} else if (commandNum == 8) {
-						motorC.setSpeed(300);// 设置电机C的转速
-						motorB.setSpeed(300);// 设置电机B的转速
-						
-					} else if(commandNum == 2 || commandNum == 4) {
-						// 颜色探测
-						 float colorNum=ColorDetection.getColorDetection();
-						 LCD.drawString("color:"+String.valueOf(colorNum), 0, 5);
-						
-					}
-				}
 			}
-			Delay.msDelay(1000);// 延迟1秒
+
+			if (i == 1) {
+				motorC.setSpeed(300);// 设置电机C的转速
+				motorB.setSpeed(300);// 设置电机B的转速
+
+				while (Button.DOWN.isDown() != true) {
+					byte[] commandArrays = new byte[4];
+					iRSensor.getRemoteCommands(commandArrays, 0, 4);
+					// int i = 0; // 变量i 用于标记按下的次数
+					for (float commandNum : commandArrays) {
+						if (commandNum != 0) {
+							if (commandNum == 1) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.forward();// 前进
+								motorB.forward();
+
+							} else if (commandNum == 3) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.backward();// 倒退
+								motorB.backward();
+
+							} else if (commandNum == 9) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.stop();// 停止
+								motorB.stop();
+
+							} else if (commandNum == 10) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.forward();// 前进
+								motorB.forward();
+								motorC.rotate(200);// 向前 向左转
+
+							} else if (commandNum == 6) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.forward();// 前进
+								motorB.forward();
+								motorB.rotate(200);// 向前 向右转
+
+							} else if (commandNum == 7) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.backward();// 倒退
+								motorB.backward();
+								motorC.rotate(200);// 向后 向左转
+
+							} else if (commandNum == 11) {
+								LCD.clear(2);
+								LCD.drawString(String.valueOf(commandNum), 10, 2);
+								motorC.backward();// 倒退
+								motorB.backward();
+								motorB.rotate(200);// 向后 向右转
+
+							} else if (commandNum == 5) {
+								motorC.setSpeed(500);// 设置电机C的转速
+								motorB.setSpeed(500);// 设置电机B的转速
+
+							} else if (commandNum == 8) {
+								motorC.setSpeed(300);// 设置电机C的转速
+								motorB.setSpeed(300);// 设置电机B的转速
+
+							} else if (commandNum == 2 || commandNum == 4) {
+								// 颜色探测
+								float colorNum = ColorDetection.getColorDetection();
+								LCD.drawString("color:" + String.valueOf(colorNum), 0, 5);
+
+							}
+						}
+					}
+					Delay.msDelay(1000);// 延迟1秒
+
+				}
+				iRSensor.close();// 关闭传感器
+				System.exit(0);
+				break;
+
+			}
+			i--;
 
 		}
-		iRSensor.close();// 关闭传感器
-
 	}
 
 	private static Telecar telecar = new Telecar();
